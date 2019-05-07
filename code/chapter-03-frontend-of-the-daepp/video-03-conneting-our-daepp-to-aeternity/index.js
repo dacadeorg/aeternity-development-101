45bdbaf35c9ecd8fcfbd6a7cf7a67db1ad553b25
moritzfelipe <stellmacher@gmail.com>
2019-05-07 14:32:25 +0200

    Adjusted code for new aepp-skd 3.1.0

--- code/chapter-03-frontend-of-the-daepp/video-03-conneting-our-daepp-to-aeternity/index.js
+++ code/chapter-03-frontend-of-the-daepp/video-03-conneting-our-daepp-to-aeternity/index.js
@@ -1,3 +1,40 @@
+const contractSource = `
+  contract MemeVote =
+
+      record meme =
+        { creatorAddress : address,
+          url            : string,
+          name           : string,
+          voteCount      : int }
+
+      record state =
+        { memes      : map(int, meme),
+          memesLength : int }
+
+      function init() =
+        { memes = {},
+          memesLength = 0 }
+
+      public function getMeme(index : int) : meme =
+        switch(Map.lookup(index, state.memes))
+          None    => abort("There was no meme with this index registered.")
+          Some(x) => x
+
+      public stateful function registerMeme(url' : string, name' : string) =
+        let meme = { creatorAddress = Call.caller, url = url', name = name', voteCount = 0}
+        let index = getMemesLength() + 1
+        put(state{ memes[index] = meme, memesLength = index })
+
+      public function getMemesLength() : int =
+        state.memesLength
+
+      public stateful function voteMeme(index : int) =
+        let meme = getMeme(index)
+        Chain.spend(meme.creatorAddress, Call.value)
+        let updatedVoteCount = meme.voteCount + Call.value
+        let updatedMemes = state.memes{ [index].voteCount = updatedVoteCount }
+        put(state{ memes = updatedMemes })
+`;
 const contractAddress ='ct_2qcqwwXmfLmZ3a18yvnv4p8ta9HGoyHRjCDvPMvyAqkuMRwzPD';
 var client = null;
 var memeArray = [];
@@ -16,9 +53,11 @@ window.addEventListener('load', async () => {
 
   client = await Ae.Aepp();
 
-  const calledGet = await client.contractCallStatic(contractAddress, 'sophia-address', 'getMemesLength', {args: '()'}).catch(e => console.error(e));
+  const contract = await client.getContractInstance(contractSource, {contractAddress});
+  const calledGet = await contract.call('getMemesLength', [], {callStatic: true}).catch(e => console.error(e));
   console.log('calledGet', calledGet);
-  const decodedGet = await client.contractDecodeData('int', calledGet.result.returnValue).catch(e => console.error(e));
+
+  const decodedGet = await calledGet.decode().catch(e => console.error(e));
   console.log('decodedGet', decodedGet.value);
 
   renderMemes();
@@ -44,6 +83,6 @@ $('#registerBtn').click(async function(){
     index: memeArray.length+1,
     votes: 0
   })
-  //update and render meme
+
   renderMemes();
 });
